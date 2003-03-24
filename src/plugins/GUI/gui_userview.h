503f6daba7144df4633f36062224938ea2749adb
krzyzak <krzyzak>
2003-03-24 20:21:19 +0000

    .missing declaration

--- src/plugins/GUI/gui_userview.h
+++ src/plugins/GUI/gui_userview.h
@@ -1,4 +1,4 @@
-/* $Id: gui_userview.h,v 1.1 2003/03/20 10:37:07 krzyzak Exp $ */
+/* $Id: gui_userview.h,v 1.2 2003/03/24 20:21:19 krzyzak Exp $ */
 
 #ifndef GGadu_GUI_USERVIEW_H
 #define GGadu_GUI_USERVIEW_H 1
@@ -21,5 +21,7 @@ void gui_user_view_switch();
 void gui_user_view_refresh();
 
 void gui_user_view_clear();
+
+void gui_user_view_unregister(gui_protocol *gp);
 
 #endif
