d5ee905ee58802790731ad8344d35ae4b39b37ef
shaster <shaster>
2003-04-14 21:11:17 +0000

    conference segfault fix (i hope), my fault.

--- src/plugins/GUI/gui_chat.c
+++ src/plugins/GUI/gui_chat.c
@@ -1,4 +1,4 @@
-/* $Id: gui_chat.c,v 1.16 2003/04/14 20:12:12 shaster Exp $ */
+/* $Id: gui_chat.c,v 1.17 2003/04/14 21:11:17 shaster Exp $ */
 
 #include <gtk/gtk.h>
 #include <string.h>
@@ -564,17 +564,19 @@ GtkWidget *create_chat(gui_chat_session *session, gchar *plugin_name, gchar *id,
 	switch (chat_type) 
 	{
 		case CHAT_TYPE_CLASSIC: {
+			gchar *wintitleC = NULL;
 			if (k)
-				wintitle = (conference) ? confer_title : g_strdup_printf(_("Talking to %s (%s) %s"),k->nick,id, (sp ? st : ""));
+				wintitleC = (conference) ? confer_title : g_strdup_printf(_("Talking to %s (%s) %s"),k->nick,id, (sp ? st : ""));
 			else 
-				wintitle = (conference) ? confer_title : g_strdup_printf(_("Talking to %s"),id);
+				wintitleC = (conference) ? confer_title : g_strdup_printf(_("Talking to %s"),id);
 
 			chat_window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-			gtk_window_set_title(GTK_WINDOW(chat_window), wintitle);
+			gtk_window_set_title(GTK_WINDOW(chat_window), wintitleC);
 		
 			gtk_box_pack_start(GTK_BOX(vbox), vbox_in_out, TRUE, TRUE, 0);
 			gtk_box_pack_end(GTK_BOX(vbox), hbox_buttons, FALSE, FALSE, 0);
 			gtk_container_add(GTK_CONTAINER(chat_window), vbox);
+			g_free(wintitleC);
 		}
 			break;
 
@@ -814,8 +816,6 @@ GtkWidget *create_chat(gui_chat_session *session, gchar *plugin_name, gchar *id,
 	gtk_widget_grab_focus(GTK_WIDGET(input));
     
 	g_free(st);
-	g_free(title);
-	g_free(wintitle);
 
 	return vbox_in_out;
 }
