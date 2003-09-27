29dec3c5088d609d62f6cd780a6a0273d1f1b034
shaster <shaster>
2003-09-27 23:38:55 +0000

    - include sys/soundcard.h rather than linux/soundcard.h. I hope it will   resolve FreeBSD compilation issue.

--- src/plugins/sound/oss/plugin_sound_oss.c
+++ src/plugins/sound/oss/plugin_sound_oss.c
@@ -1,4 +1,4 @@
-/* $Id: plugin_sound_oss.c,v 1.3 2003/06/09 00:20:45 krzyzak Exp $ */
+/* $Id: plugin_sound_oss.c,v 1.4 2003/09/27 23:38:56 shaster Exp $ */
 
 /*
  *    Originally written by Igor Truszkowski <pol128@polsl.gliwice.pl>
@@ -16,7 +16,7 @@
 #include <sys/types.h>
 #include <sys/ioctl.h>
 #include <errno.h>
-#include <linux/soundcard.h>
+#include <sys/soundcard.h>
 
 #include "gg-types.h"
 #include "unified-types.h"
