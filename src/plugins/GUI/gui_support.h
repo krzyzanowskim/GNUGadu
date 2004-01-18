3cf26f38d25b289b45f40f04d6fec78161f20fa0
krzyzak <krzyzak>
2004-01-18 12:14:04 +0000

    - switch classic/tabbed availavle only if there is no one session active.

--- src/plugins/GUI/gui_support.h
+++ src/plugins/GUI/gui_support.h
@@ -1,4 +1,4 @@
-/* $Id: gui_support.h,v 1.3 2004/01/17 00:44:59 shaster Exp $ */
+/* $Id: gui_support.h,v 1.4 2004/01/18 12:14:05 krzyzak Exp $ */
 
 #ifndef GGadu_GUI_PLUGIN_FIND_H
 #define GGadu_GUI_PLUGIN_FIND_H 1
@@ -13,6 +13,8 @@ gui_chat_session *gui_session_find_confer(gui_protocol * gp, GSList * recipients
 
 /* obsolete */
 gui_chat_session *gui_session_find(gui_protocol * gp, gchar * id);
+
+gboolean gui_check_for_sessions(GSList * protocolsl);
 
 gui_protocol *gui_find_protocol(gchar * plugin_name, GSList * protocolsl);
 
