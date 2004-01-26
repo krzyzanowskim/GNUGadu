6a5e66208d6ad358746b891e8a06c827165aee0e
krzyzak <krzyzak>
2004-01-26 19:16:13 +0000

    - gettext ? hmmmmmmm should working

--- autogen.sh
+++ autogen.sh
@@ -46,26 +46,30 @@ if automake-1.7 --version < /dev/null > /dev/null 2>&1 ; then
     AUTOMAKE=automake-1.7
     ACLOCAL=aclocal-1.7
 else
+    if automake-1.8 --version < /dev/null > /dev/null 2>&1 ; then
+	AUTOMAKE=automake-1.8
+        ACLOCAL=aclocal-1.8
+    else
         echo
-        echo "You must have automake 1.7.x installed to compile $PROJECT."
+        echo "You must have at least automake 1.7.x installed to compile $PROJECT."
         echo "Install the appropriate package for your distribution,"
         echo "or get the source tarball at http://ftp.gnu.org/gnu/automake/"
 	echo "\n"
 	echo "trying to automake anyway but YOU WERE WARNED"
 	AUTOMAKE=automake
     	ACLOCAL=aclocal
+    fi
 fi
-
 
 if test "$DIE" -eq 1; then 
         exit 1
 fi
 
 echo "gettextize"
-gettextize -f
+gettextize --force --copy --no-changelog || exit 1
 
 echo "aclocal"
-$ACLOCAL $ACPATH || exit 1
+$ACLOCAL $ACPATH -I m4 || exit 1
 
 echo "libtoolize"
 libtoolize --force --copy --automake || exit 1
