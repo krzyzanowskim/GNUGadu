28dce04bb61a5c4e4cd707fda803c32c0ddc2589
krzyzak <krzyzak>
2004-02-17 09:56:08 +0000

    - enable blink by default

--- src/plugins/GUI/gui_main.c
+++ src/plugins/GUI/gui_main.c
@@ -1,4 +1,4 @@
-/* $Id: gui_main.c,v 1.45 2004/02/17 09:29:54 krzyzak Exp $ */
+/* $Id: gui_main.c,v 1.46 2004/02/17 09:56:09 krzyzak Exp $ */
 
 /* 
  * GUI (gtk+) plugin for GNU Gadu 2 
@@ -172,7 +172,7 @@ GGaduPlugin *initialize_plugin(gpointer conf_ptr)
 					  (gpointer) DEFAULT_CHAT_WINDOW_WIDTH);
 	ggadu_config_var_add_with_default(gui_handler, "chat_window_height", VAR_INT,
 					  (gpointer) DEFAULT_CHAT_WINDOW_HEIGHT);
-	ggadu_config_var_add(gui_handler, "blink", VAR_BOOL);
+	ggadu_config_var_add_with_default(gui_handler, "blink", VAR_BOOL,(gpointer) TRUE);
 	ggadu_config_var_add(gui_handler, "blink_interval", VAR_INT);
 	ggadu_config_var_add(gui_handler, "auto_away", VAR_BOOL);
 	ggadu_config_var_add(gui_handler, "auto_away_interval", VAR_INT);
