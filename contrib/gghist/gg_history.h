31d424d1d81f60fa863fa0f9eba3444c548b24e0
krzyzak <krzyzak>
2004-08-02 09:23:18 +0000

    - header file

--- contrib/gghist/gg_history.h
+++ contrib/gghist/gg_history.h
@@ -4,92 +4,15 @@
 #ifndef GG_HISTORY_SUPPORT
 #define GG_HISTORY_SUPPORT
 
+#include <sys/types.h>
+#include <unistd.h>
+#include <glib.h>
+
 #define GG_HIST_SEND 1
 #define GG_HIST_RCV 2 
 #define GG_HIST_STATUS 3
 
 #define SHOW_STATUS
-
-char *getitem(int fd, int offset, char schr, char echr, int item, int cut)
-{
-    int	i	= 0,
-	found	= 0,
-	start	= -1-item,
-	end	= -1-item;
-    char *tmp	= calloc(2, sizeof(char));
-    
-    if(schr == '\0') start = 0;
-    if(echr == '\n') end   = -1;
-    
-    lseek(fd, offset, 0);
-    
-    while(!found)
-    {
-	read(fd, tmp, 1);
-	
-	if(tmp[0] == schr)
-	{
-	    if(start == -1) start = i;
-	    else if(start < -1) start++;
-	}
-	
-	if(tmp[0] == echr)
-	{
-	    if((end == -1)&&(start != i)) end = i;
-	    else if(end < -1) end++;
-	}
-	
-	if((start > -1)&&(end > -1)) found++;
-	i++;
-    }
-    
-    free(tmp);
-    if(!found) return NULL;
-    
-    tmp = calloc(end-start+1, sizeof(char));
-    lseek(fd, offset+start+cut, 0);
-    read(fd, tmp, end-start-cut);
-    
-    return tmp;
-}
-
-int lines_count(int fd)
-{
-    char *tmp = calloc(2, sizeof(char));
-    int lines = 0;
-    
-    lseek(fd, 0, 0);
-    while(1)
-    {
-	if(read(fd, tmp, 1) != 1) break;
-	if(tmp[0] == '\n') lines++;
-    }
-    free(tmp);
-
-    return lines++;
-}
-
-int get_lines(int fd, int *list)
-{
-    char *tmp = calloc(2, sizeof(char));
-
-    int  i = 0,
-	 pos = 0;
-	
-
-    list[0] = 0;
-    lseek(fd, 0, 0);
-    while(1)
-    {
-	if(read(fd, tmp, 1) != 1) break;
-	if(tmp[0] == '\n')
-	    list[1+i++] = pos+1;
-	pos++;
-    }
-    free(tmp);
-    
-    return i;
-}
 
 struct gg_hist_line
 {
@@ -102,105 +25,11 @@ struct gg_hist_line
     int type;
 };
 
-
-struct gg_hist_line *formatline(int fd, int offset)
-{
-    struct gg_hist_line *hist_line = calloc(1, sizeof(struct gg_hist_line));
-    int msg_type = 0;
-    
-    char *type = getitem(fd, offset, '\0', ',', 0, 0);
-    
-    if(strcmp(type, "chatsend") == 0)		msg_type = GG_HIST_SEND;
-    else if(strcmp(type, "chatrcv") == 0)	msg_type = GG_HIST_RCV;
-    else if(strcmp(type, "status") == 0)	msg_type = GG_HIST_STATUS;
-    free(type);
-    hist_line->type = msg_type;
-    
-    switch(msg_type)
-    {
-	case GG_HIST_SEND:
-	{
-	    char *tmp = NULL;
-	    
-	    tmp = getitem(fd, offset, ',', ',', 0, 1);
-	    hist_line->id = tmp; // ID
-	    tmp = getitem(fd, offset, ',', ',', 1, 1);
-	    hist_line->nick = tmp; // NICK
-	    tmp = getitem(fd, offset, ',', ',', 2, 1);
-	    hist_line->timestamp1 = tmp; // TIMESTAMP1
-	    tmp = getitem(fd, offset, ',', '\n', 3, 1);
-	    hist_line->data = tmp; // DATA
-
-	    break;
-	}
-	case GG_HIST_RCV:
-	{
-	    char *tmp = NULL;
-
-	    tmp = getitem(fd, offset, ',', ',', 0, 1);
-	    hist_line->id = tmp; // ID
-	    tmp = getitem(fd, offset, ',', ',', 1, 1);
-	    hist_line->nick = tmp; // NICK
-	    tmp = getitem(fd, offset, ',', ',', 2, 1);
-	    hist_line->timestamp1 = tmp; // TIMESTAMP1
-	    tmp = getitem(fd, offset, ',', ',', 3, 1);
-	    hist_line->timestamp2 = tmp; // TIMESTAMP2
-	    tmp = getitem(fd, offset, ',', '\n', 4, 1);
-	    hist_line->data = tmp; // DATA
-
-	    break;
-	}
-	case GG_HIST_STATUS:
-	{
-	    char *tmp = NULL;
-	    
-	    tmp =  getitem(fd, offset, ',', ',', 0, 1);
-	    hist_line->id = tmp; // ID
-	    tmp = getitem(fd, offset, ',', ',', 1, 1);
-	    hist_line->nick = tmp; // NICK
-	    tmp = getitem(fd, offset, ',', ',', 2, 1);
-	    hist_line->ip = tmp; // IP
-	    tmp = getitem(fd, offset, ',', ',', 3, 1);
-	    hist_line->timestamp1 = tmp; // TIMESTAMP1
-	    tmp = getitem(fd, offset, ',', '\n', 4, 1);
-	    hist_line->data = tmp; // DATA
-	    
-	    break;
-	}
-    }
-    
-    return hist_line;
-}
-
-gchar *gg_convert_utf8(char *data)
-{
-    gchar *tmp = g_convert(data, strlen(data), "UTF-8", "ISO8859-2", NULL, NULL, NULL);
-    
-    if(g_utf8_validate(tmp, strlen(tmp), NULL) == FALSE)
-    {
-	g_free(tmp);
-	tmp = g_convert(data, strlen(data), "UTF-8", "CP1250", NULL, NULL, NULL);
-    }
-    
-    if(g_utf8_validate(tmp, strlen(tmp), NULL) == FALSE)
-	g_warning("Could not convert to UTF-8!\n");
-    
-    return tmp;
-}
-
-gchar *gg_hist_time(int timestamp)
-{
-    gchar   gtmp[32],
-	    *gtmp2;
-    
-    struct tm *ptm = (struct tm *)localtime((time_t *)&timestamp);
-    strftime(gtmp, sizeof(gtmp), "%H:%M:%S (%Y-%m-%d)", ptm);
-    free(ptm);
-    
-    gtmp2 = g_strdup_printf("%s", gtmp);
-    
-    return gtmp2;
-}
-
+char *getitem(int fd, int offset, char schr, char echr, int item, int cut);
+int lines_count(int fd);
+int get_lines(int fd, int *list);
+struct gg_hist_line *formatline(int fd, int offset);
+gchar *gg_convert_utf8(char *data);
+gchar *gg_hist_time(int timestamp);
 
 #endif
