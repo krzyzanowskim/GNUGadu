f188b76417e4fe4246a5575cf4aeab3310e5e342
krzyzak <krzyzak>
2004-10-25 14:18:31 +0000

    - dbus-send example

--- src/plugins/dbus/build_test.sh
+++ src/plugins/dbus/build_test.sh
@@ -1,1 +1,4 @@
+# dbus-send example
+# dbus-send --system /im/freedesktop/org im.freedesktop.org.Signal.Ping string:'aaaaa'
+
 gcc `pkg-config --libs dbus-glib-1 dbus-1 glib-2.0` `pkg-config --cflags dbus-glib-1 dbus-1 glib-2.0` test.c -o test
