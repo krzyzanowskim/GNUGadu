fa1d91c77cc0357f1f908043f1d04d2228da3b07
krzyzak <krzyzak>
2004-10-29 09:04:13 +0000

    - return method exapmple added

--- src/plugins/dbus/test.c
+++ src/plugins/dbus/test.c
@@ -30,6 +30,8 @@ int main(int argc, char **argv)
 
 	/* get avaiable protocols */
 	get_protocols(bus);
+	/* open chat window */
+	open_chat(bus);
 	/* Set up this connection to work in a GLib event loop */
 	dbus_connection_setup_with_g_main(bus, NULL);
 	/* Every second call send_ping() with the bus as an argument */
@@ -51,12 +53,10 @@ static gboolean send_ping(DBusConnection *bus)
 					       DBUS_ORG_FREEDESKTOP_IM_GET_PRESENCE);
 					       
 	dbus_message_append_args(message, DBUS_TYPE_STRING, "gg://5074881", DBUS_TYPE_INVALID);
-//	dbus_message_set_no_reply(message, TRUE);
 	/* Send the signal */
 	dbus_connection_send(bus, message, NULL);
 	/* Free the signal now we have finished with it */
 	dbus_message_unref(message);
-	open_chat(bus);
 	/* Tell the user we send a signal */
 	/* Return TRUE to tell the event loop we want to be called again */
 	return TRUE;
@@ -64,16 +64,37 @@ static gboolean send_ping(DBusConnection *bus)
 
 static void get_protocols(DBusConnection *bus)
 {
-	DBusMessage *message;
+	DBusMessage *message = NULL;
+	DBusMessage *reply = NULL;
+	DBusPendingCall *pending_call;
+	DBusMessageIter iter;
 
 	message = dbus_message_new_method_call(DBUS_ORG_FREEDESKTOP_IM_SERVICE, 
 					       DBUS_ORG_FREEDESKTOP_IM_OBJECT,
 					       DBUS_ORG_FREEDESKTOP_IM_INTERFACE,
 					       DBUS_ORG_FREEDESKTOP_IM_GET_PROTOCOLS);
 	
-	dbus_connection_send(bus, message, NULL);
+	/*dbus_connection_send(bus, message, NULL);*/
+	dbus_connection_send_with_reply(bus,message,&pending_call,-1);
+	dbus_pending_call_block (pending_call);
+	reply = dbus_pending_call_get_reply (pending_call);
+	
+	/* process reply */
+	dbus_message_iter_init(reply,&iter);
+	do {
+	    gchar *protocol = NULL;
+	    if (dbus_message_iter_get_arg_type (&iter) != DBUS_TYPE_STRING)
+	          break;
+		  
+	    protocol = g_strdup (dbus_message_iter_get_string (&iter));
+	    g_print("return getProtocols: %s\n",protocol);
+	    g_free(protocol);
+	    
+	} while (dbus_message_iter_next (&iter));
 
 	dbus_message_unref(message);
+	dbus_message_unref(reply);
+	dbus_pending_call_unref (pending_call);
 }
 
 static void open_chat(DBusConnection *bus)
